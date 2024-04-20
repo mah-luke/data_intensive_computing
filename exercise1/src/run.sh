4c66dc6c1be80e5a16886bdb84efed0c395ed4dd
Lukas <lukas.mah@outlook.at>
2024-04-20 18:20:15 +0200

    fix path to run.py to be dependent on run.sh path

--- exercise1/src/run.sh
+++ exercise1/src/run.sh
@@ -3,6 +3,6 @@
 # This script starts the chi square calculation program on a cluster and writes the result to stdout
 # also
 
-/sw/venv/python39/dic24/bin/python run.py \
+/sw/venv/python39/dic24/bin/python "$(dirname "$0")/run.py" \
     --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar \
     -r hadoop hdfs:///user/dic24_shared/amazon-reviews/full/reviewscombined.json
